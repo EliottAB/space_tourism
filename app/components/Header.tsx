9a0fdd328b8dae3cf69ba2b117700bf9c7aac052
Abadie Eliott <eliott.abadiesouchar@icloud.com>
2023-08-27 16:01:25 +0200

    home page content + css

---
+++ app/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import "../css/components/header.css"
+import Logo from "../assets/shared/logo.svg"
+
+export const Header = ({page}: {page: 'home' | 'destination' | 'crew' | 'technology'}) => {
+  return (
+    <header>
+        <Image className="logo" src={Logo} alt="Logo"/>
+
+        <div className='header-line'></div>
+
+        <nav className='barlow'>
+            <Link href={"/"} className={page === "home" ? "selected-page" : ""}><span className='nav-index'>01</span> HOME</Link>
+            <Link href={"/destination"} className={page === "destination" ? "selected-page" : ""}><span className='nav-index'>02</span> DESTINATION</Link>
+            <Link href={"/crew"} className={page === "crew" ? "selected-page" : ""}><span className='nav-index'>03</span> CREW</Link>
+            <Link href={"/technology"} className={page === "technology" ? "selected-page" : ""}><span className='nav-index'>04</span> TECHNOLOGY</Link>
+        </nav>
+    </header>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,31 @@
+import Link from "next/link"
+import { Header } from "./components/Header"
+import Image from "next/image"
+import { Fragment } from "react"
+import "./css/pages/home.css"
+import homeBackDesktop from "./assets/home/background-home-desktop.jpg"
+import homeBackTablet from "./assets/home/background-home-tablet.jpg"
+import homeBackMobile from "./assets/home/background-home-mobile.jpg"
+
 export default function Home() {
   return (
-    <main>
-    </main>
+    <Fragment>
+      <Header page="home"/>
+      <main className="home-main">
+
+        <div className="presentation">
+          <h2 className="barlow">SO, YOU WANT TO TRAVEL TO</h2>
+          <h1 className="bellefair">SPACE</h1>
+          <p className="barlow">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+        </div>
+
+        <Link href={"/destination"} className="explore-link bellefair">EXPLORE</Link>
+
+      </main>
+
+      <Image className="background-image desktop" src={homeBackDesktop} alt="Earth in space"/>
+      <Image className="background-image tablet" src={homeBackTablet} alt="Earth in space"/>
+      <Image className="background-image mobile" src={homeBackMobile} alt="Earth in space"/>
+    </Fragment>
   )
 }
